7d20e59d39bef7c48485ded79968722e2bd3bcb6
timglabisch <tim@ainfach.de>
2023-12-30 22:42:42 +0100

    ...

---
+++ crates/easyprot/src/parser/parser.rs
@@ -0,0 +1,38 @@
+use nom::bytes::complete::tag;
+use nom::{complete, IResult, Parser};
+use nom::character::complete::space0;
+
+
+pub fn parse(s: &str) -> IResult<&str, Vec<Message>> {
+    ::nom::multi::many1(parse_message).parse(s)
+}
+
+struct Message {}
+
+
+pub fn parse_message(s: &str) -> IResult<&str, Message> {
+    let (s, _) = space0.parse(s)?;
+    let (s, _) = tag("Message").parse(s)?;
+    let (s, _) = space0.parse(s)?;
+    let (s, _) = tag("{").parse(s)?;
+    let (s, _) = space0.parse(s)?;
+    let (s, _) = tag("}").parse(s)?;
+    let (s, _) = space0.parse(s)?;
+
+    Ok((s, Message {}))
+}
+
+
+#[test]
+fn parse_empty() -> Result<(), ::anyhow::Error> {
+    let (a, b) = parse("Message{}Message{}")?;
+    assert_eq!(b.len(), 2);
+    Ok(())
+}
+
+#[test]
+fn parse_whitespace() -> Result<(), ::anyhow::Error> {
+    let (a, b) = parse("Message { } Message { } ")?;
+    assert_eq!(b.len(), 2);
+    Ok(())
+}
